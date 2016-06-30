0413eb848ef14390b76c8b481693584ba60dbf97
sheli16 <sheli16@live.seminolestate.edu>
2016-06-29 23:32:54 -0400

    everyting setup, tweek code, work on data function

---
+++ app/data/friends.js
@@ -0,0 +1,73 @@
+// Star Wars Characters (DATA)
+// =============================================================
+var characters = [
+
+	{
+  "name":"Toni",
+  "photo":"https://img.buzzfeed.com/buzzfeed-static/static/2014-06/12/14/enhanced/webdr03/enhanced-26006-1402598897-17.jpg?no-auto",
+  "scores":[
+     3,
+     3,
+     1,
+     6,
+     5,
+     1,
+     1,
+     1,
+     5,
+     1
+   ]
+}
+	{
+  "name":"Jimi",
+  "photo":"https://img.buzzfeed.com/buzzfeed-static/static/2014-06/12/14/enhanced/webdr08/enhanced-1823-1402599162-17.jpg?no-auto",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+},
+
+		{
+  "name":"Nola",
+  "photo":"https://img.buzzfeed.com/buzzfeed-static/static/2014-06/12/14/enhanced/webdr06/enhanced-2873-1402597070-9.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+}
+
+{
+  "name":"Fred",
+  "photo":"https://img.buzzfeed.com/buzzfeed-static/static/2014-06/12/14/enhanced/webdr04/enhanced-7850-1402597428-9.jpg?no-auto",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+}
+
+]
+
---
+++ app/routing/api-routes.js
@@ -0,0 +1,204 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources. 
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendslist 	= require('../data/friends.js');
+// var waitListData 	= require('../data/waitinglist-data.js');
+var path 			= require('path');
+
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app){
+
+	// API GET Requests
+	// Below code handles when users "visit" a page. 
+	// In each of the below cases when a user visits a link 
+	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table) 
+	// ---------------------------------------------------------------------------
+
+	app.get('/api/friends', function(req, res){
+		res.json(friendslist);
+	});
+
+		// API POST Requests
+	// Below code handles when a user submits a form and thus submits data to the server.
+	// In each of the below cases, when a user submits form data (a JSON object)
+	// ...the JSON is pushed to the appropriate Javascript array
+	// (ex. User fills out a reservation request... this data is then sent to the server...
+	// Then the server saves the data to the tableData array)
+	// ---------------------------------------------------------------------------
+
+	app.post('/api/friends', function(req, res){
+
+		// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+		// It will do this by sending out the value "true" have a table 
+		if(friendslist.length < 5 ){
+			friendslist.push(req.body);
+			res.json(true); // KEY LINE -In order to rack that they made the reservation
+		}
+
+		// Or false if they don't have a table
+		else{
+			waitListData.push(req.body);
+			res.json(false); // KEY LINE
+		}
+
+	});
+
+	// ---------------------------------------------------------------------------
+	// I added this below code so you could clear out the table while working with the functionality.
+	// Don't worry about it!
+
+	app.post('/api/clear', function(req, res){
+		// Empty out the arrays of data
+		friendslist = [];
+		
+
+		console.log(friendslist);
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, 'view.html'));
+})
+
+app.get('/add', function(req, res){
+	res.sendFile(path.join(__dirname, 'add.html'));
+})
+
+app.get('/all', function(req, res){
+	res.sendFile(path.join(__dirname, 'all.html'));
+})
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// --------------------not sure if I need this portion------------------------------------
+
+// Search for Specific Character (or all characters) - provides JSON
+app.get('/api/:characters?', function(req, res){
+
+	var chosen = req.params.characters;
+
+	if(chosen){
+		console.log(chosen);
+
+		for (var i=0; i <characters.length; i++){
+
+			if (chosen == characters[i].routeName){
+				res.json(characters[i]);
+				return;
+			}
+		}
+
+		res.json(false);
+	}
+
+	else{
+		res.json(characters);
+	}
+})
+
+// Create New Characters - takes in JSON input
+app.post('/api/new', function(req, res){
+
+	var newcharacter = req.body;
+	newcharacter.routeName = newcharacter.name.replace(/\s+/g, '').toLowerCase()
+
+	console.log(newcharacter);
+
+	characters.push(newcharacter);
+
+	res.json(newcharacter);
+})
+
